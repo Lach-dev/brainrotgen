450c24ba2811f7a27b68584e8093e2cc0d4636f2
Nathan Levites <nathan@hiddenlayer.com>
2024-09-10 20:54:55 -0700

    f

--- config.py
+++ config.py
@@ -8,3 +8,5 @@ class Config:
     REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
     REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
     REDDIT_USER_AGENT = os.getenv("REDDIT_USER_AGENT")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    OPENAI_MODEL = os.getenv("OPENAI_MODEL")
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from reddit_scraper import scrape_posts
+from script_generator import ScriptGenerator
+import logging
 
+logging.basicConfig(level=logging.INFO)
+
+script_gen = ScriptGenerator()
 
 def main():
+    post_count = 5
+
+    logging.info(f"Scraping {post_count} posts from reddit...")
     reddit_posts = scrape_posts(5)
-    for post in reddit_posts:
-        print(post.author)
-        print(post.title)
-    # generate script from reddit post
-    # generate audio from script
+    logging.info(f"Scraped {len(reddit_posts)} posts from reddit.")
+
+    logging.info("Generating script from reddit post...")
+    script, audio_path = script_gen.generate_script(reddit_posts[0])
+    logging.info("Generated script from reddit post.")
+
     # cut videos and put together 4-1
     # overlay audio, text on top of video
     # upload to instagram reels
